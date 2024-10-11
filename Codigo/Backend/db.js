2fc2b0e0e73ada2316c9b13b7784b787454ad43c
Mumoide <restevez1993@gmail.com>
2024-10-10 22:31:34 -0300

    fixeando

--- Codigo/Backend/db.js
+++ Codigo/Backend/db.js
@@ -3,14 +3,14 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'MedicalAidDB', 
-  password: '202020',
+  database: 'Medical-aid',
+  password: 'mumo123',
   port: 5432,
 });
 
 pool.connect((err, client, release) => {
   if (err) {
-      return console.error('Error acquiring client', err.stack);
+    return console.error('Error acquiring client', err.stack);
   }
   console.log('Connected to the database');
   release();
