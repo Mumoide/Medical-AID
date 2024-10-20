a82b33db39c1734633b569dccdec4d4a0df8a145
Imperial-Marv <marcel_brard@outlook.com>
2024-10-20 00:42:07 -0300

    fix: added env var to sequelize config

---
+++ Codigo/Backend/config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();  // Load environment variables from .env file
+
+module.exports = {
+  development: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT || 5432, // Default to 5432 if not specified
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT || 5432,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT || 5432,
+    dialect: "postgres"
+  }
+};
