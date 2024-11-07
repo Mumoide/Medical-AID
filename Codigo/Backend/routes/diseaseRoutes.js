ff7e81e59c0b2d6a00de1b8f4f4c7b39aceb47dc
Mumoide <restevez1993@gmail.com>
2024-11-07 19:26:18 -0300

    feat: getAllDiseases endpoint added

--- Codigo/Backend/routes/diseaseRoutes.js
+++ Codigo/Backend/routes/diseaseRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getDiseaseByModelOrder } = require('../controllers/DiseaseController.js'); // Import the controller
+const { getDiseaseByModelOrder, getAllDiseases } = require('../controllers/DiseaseController.js'); // Import the controller
+
 
 // Define the route for fetching disease by model_order
 router.get('/:model_order', getDiseaseByModelOrder); // Use the controller function
+
+// Route to get all diseases
+router.get('/diseases', getAllDiseases);
 
 module.exports = router;
