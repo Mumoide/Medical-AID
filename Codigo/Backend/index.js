5fe2655fbdc465d0ca9b57cc834a387c79b72e30
Imperial-Marv <marcel_brard@outlook.com>
2024-11-13 23:48:01 -0300

    feat: added local ips access in backend

--- Codigo/Backend/index.js
+++ Codigo/Backend/index.js
@@ -15,11 +15,25 @@ const newsletterRoutes = require('./routes/subscribeNewsLetterRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const corsOptions = {
-  origin: 'http://localhost:3000', // Allow the front-end domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  origin: (origin, callback) => {
+    // List allowed origins
+    const allowedOrigins = ['http://localhost:3000'];
+    
+    // Check if the origin matches localhost or falls within the IP range
+    if (
+      allowedOrigins.includes(origin) ||
+      /^http:\/\/192\.168\.1\.(\d|[1-9]\d|1\d\d|2[0-5][0-5]):3000$/.test(origin) // Regex for IP range 192.168.1.1 to 192.168.1.255
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
+
 // Middleware configuration
 app.use(cors(corsOptions));
 app.use(express.json());
